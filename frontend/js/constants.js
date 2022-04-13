547b460c16f723e06f7a2e2d0b67c1af284e2f49
Northdesire <nic.iburg@yahoo.de>
2022-04-13 12:16:03 +0200

    hey

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x66cF1De878D0398C3847Ac5AeeABD029637E2117"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Moin!";
